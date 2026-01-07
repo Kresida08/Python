ab1f8dfcf6f9b3073789ad24a10798447302e930
Cressida Fernandes <fernandescressida8@gmail.com>
2026-01-07 12:41:58 +0530

    Add files via upload

---
+++ start.py
@@ -0,0 +1,80 @@
+# print("start")
+# marks= [12,18,13,21,10]
+# for i in marks :
+#      i= i+2
+#      print (i)
+
+
+# for i in range (8):
+#     print (i)
+
+# for i in range (1,8):
+#   print (i)
+
+# count = 1
+# while count<=5:
+#     print (count)
+#     count+=1
+
+# for i in range (1,5):
+#   for j in range (1,11):
+#     print(i*j)
+
+
+# for i in range (1,6):
+#     if i == 3:
+#         break 
+#     print (i)
+
+
+# for i in range (1,6):
+#     if i == 3:
+#         continue
+#     print (i)
+
+# for i in range(1,100):
+#     if i%2==0:
+#         continue
+#     else:
+#      print(i)
+
+
+# for i in range(1,101):
+#     if i%2!=0:
+#         continue
+#     print(i)
+
+# lst = [12,45,67,98,54]
+# for i in lst:
+#     if i%2==0:
+#         print("even",i)
+#     else:
+#         print("odd",i)    
+
+    
+
+# age=int(input("Enter your age"))
+# if age>18:
+#     print ("you can vote")
+
+
+#     age = int (input("enter your age"))
+#     if age>18:
+#         print ("you can vote")
+#     else:
+#         print ("you can't vote")
+
+# for i in range (1,9):
+#     i%3==0
+#     break
+# print(i)
+# i+=1
+
+# i=0
+# while i<5:
+#     print(i)
+#     i+=1
+#     if i==3:
+#      break
+# else:
+#  print(0)
